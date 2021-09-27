13acdeba9afc1d0fb476114116852c3d0d5bbcc4
Maunaloa576 <vanja.stanimirovic.4057@metropolitan.ac.rs>
2021-09-27 13:55:47 +0200

    izmena

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import { CartContextProvider } from './store/cart-context';
 
 ReactDOM.render(<App />, document.getElementById('root'));
